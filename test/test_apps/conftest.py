eb7f818a4ab605a3aed553880a85f5e05afea295
Peter Dragun <peter.dragun@espressif.com>
2023-08-04 14:42:38 +0200

    feat(target_tests): add ci configuration

---
+++ test/test_apps/conftest.py
@@ -0,0 +1,76 @@
+import logging
+import os
+import sys
+from datetime import datetime
+from typing import Callable, Optional
+
+import pytest
+from pytest_embedded.plugin import multi_dut_argument, multi_dut_fixture
+
+DEFAULT_SDKCONFIG = 'default'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def session_tempdir() -> str:
+    _tmpdir = os.path.join(
+        os.path.dirname(__file__),
+        'pytest_embedded_log',
+        datetime.now().strftime('%Y-%m-%d_%H-%M-%S'),
+    )
+    os.makedirs(_tmpdir, exist_ok=True)
+    return _tmpdir
+
+
+@pytest.fixture
+@multi_dut_argument
+def config(request: pytest.FixtureRequest) -> str:
+    return getattr(request, 'param', None) or DEFAULT_SDKCONFIG
+
+
+@pytest.fixture
+@multi_dut_fixture
+def build_dir(request: pytest.FixtureRequest, app_path: str, target: Optional[str], config: Optional[str]) -> str:
+    """
+    Check local build dir with the following priority:
+
+    1. build_<target>_<config>
+    2. build_<target>
+    3. build_<config>
+    4. build
+
+    Returns:
+        valid build directory
+    """
+    check_dirs = []
+    if target is not None and config is not None:
+        check_dirs.append(f'build_{target}_{config}')
+    if target is not None:
+        check_dirs.append(f'build_{target}')
+    if config is not None:
+        check_dirs.append(f'build_{config}')
+    check_dirs.append('build')
+
+    for check_dir in check_dirs:
+        binary_path = os.path.join(app_path, check_dir)
+        if os.path.isdir(binary_path):
+            logging.info(f'find valid binary path: {binary_path}')
+            return check_dir
+
+        logging.warning(
+            'checking binary path: %s... missing... try another place', binary_path
+        )
+
+    recommend_place = check_dirs[0]
+    logging.error(
+        f'no build dir valid. Please build the binary via "idf.py -B {recommend_place} build" and run pytest again'
+    )
+    sys.exit(1)
+
+
+@pytest.fixture(autouse=True)
+@multi_dut_fixture
+def junit_properties(test_case_name: str, record_xml_attribute: Callable[[str, object], None]) -> None:
+    """
+    This fixture is autoused and will modify the junit report test case name to <target>.<config>.<case_name>
+    """
+    record_xml_attribute('name', test_case_name)
