b7369405c3060386009775a7dfe1e553152be918
Peter Dragun <peter.dragun@espressif.com>
2024-01-30 08:54:29 +0100

    change: Update version to 1.4.0

--- esp_idf_monitor/__init__.py
+++ esp_idf_monitor/__init__.py
@@ -1,4 +1,4 @@
 from .base.ansi_color_converter import ANSIColorConverter  # noqa: F401
 from .base.ansi_color_converter import get_ansi_converter  # noqa: F401
 
-__version__ = '1.3.4'
+__version__ = '1.4.0'
