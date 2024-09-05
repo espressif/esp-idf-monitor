707c30115c465442a83db96ae4ce2760644b569c
Peter Dragun <peter.dragun@espressif.com>
2024-09-05 15:50:40 +0200

    fix: improve error message when STDIN in not attached to TTY

--- esp_idf_monitor/idf_monitor.py
+++ esp_idf_monitor/idf_monitor.py
@@ -346,7 +346,7 @@ def detect_port() -> Union[str, NoReturn]:
 
 def main() -> None:
     if not sys.stdin.isatty() and not os.environ.get('ESP_IDF_MONITOR_TEST'):
-        sys.exit('error: Monitor requires standard input to be attached to TTY')
+        sys.exit('Error: Monitor requires standard input to be attached to TTY. Try using a different terminal.')
     parser = get_parser()
     args = parser.parse_args()
 
